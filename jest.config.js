f7379cc31a6feb88e8b6c5f45e9f8cc5dca2fcfb
Odravison <oamaraljr@gmail.com>
2021-08-21 03:02:42 +0100

    Initial structure for eslint, jest, prettier, express, orm, typescript, vscode debug

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // A preset that is used as a base for Jest's configuration
+  preset: 'ts-jest',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node'
+};
