d809dd9f43747d7a697b45e8cf1d795d5ba7420b
Odravison <oamaraljr@gmail.com>
2021-08-24 04:04:53 +0100

    Removes unused configuration from jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,13 @@
 module.exports = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
-
-  // watchPathIgnorePatterns: [
-  //   "<rootDir>/src/__tests__/ConstUtils.ts",
-  //   "<rootDir>/src/__tests__/TestUtils.ts",
-  //   "<rootDir>/src/__tests__/api/TaskApi.spec.ts"
-  // ],
   testPathIgnorePatterns: [
     "<rootDir>/src/__tests__/ConstUtils.ts",
     "<rootDir>/src/__tests__/TestUtils.ts",
     "<rootDir>/src/__tests__/api/TaskApi.spec.ts"
   ],
-  // coveragePathIgnorePatterns: [
-  //   "<rootDir>/src/__tests__/ConstUtils.ts",
-  //   "<rootDir>/src/__tests__/TestUtils.ts",
-  //   "<rootDir>/src/__tests__/api/TaskApi.spec.ts"
-  // ],
-
   // A preset that is used as a base for Jest's configuration
   preset: 'ts-jest',
-
   // The test environment that will be used for testing
   testEnvironment: 'node'
 };
