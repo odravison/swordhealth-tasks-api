5d8c3d5c78eb4cbb265e86cb6df7172776db97ef
Odravison <oamaraljr@gmail.com>
2021-08-25 00:50:01 +0100

    Create AbstractService to demonstrate two concepts

---
+++ src/services/AbstractService.ts
@@ -0,0 +1,55 @@
+import { DeepPartial, Repository } from 'typeorm';
+import AppError from '../errors/AppError';
+import BasicEntity from '../models/BasicEntity';
+
+abstract class AbstractService<T extends BasicEntity> {
+  abstract getRepository(): Repository<T>;
+
+  async findById(
+    id: string | number | Date | undefined,
+  ): Promise<T | undefined> {
+    if (id === undefined) {
+      console.error('Find entity error. Id undefined');
+      throw new AppError('Id cannot be undefined', 400);
+    }
+    const entityFound: T | undefined = await this.getRepository().findOne(id);
+    if (entityFound === undefined) {
+      console.error('Find entity error. Entity: ', entityFound);
+      throw new AppError('Entity not found', 404);
+    }
+
+    return entityFound;
+  }
+
+  async remove(id: string | number | Date | undefined): Promise<T> {
+    if (id === undefined) {
+      console.error('Remove entity error. Id undefined');
+      throw new AppError('Id cannot be undefined', 400);
+    }
+    const entityFound: T | undefined = await this.findById(id);
+    if (entityFound === undefined) {
+      console.error('Remove entity error. Entity: ', entityFound);
+      throw new AppError('Entity not found', 404);
+    }
+    return this.getRepository().remove(entityFound);
+  }
+
+  async save(entity: DeepPartial<T>): Promise<T> {
+    if (entity.id !== undefined) {
+      console.error('Save entity error. Entity: ', entity);
+      throw new AppError('Entity could not be saved', 400);
+    }
+    return this.getRepository().save(entity);
+  }
+
+  async update(entity: DeepPartial<T>): Promise<T> {
+    if (entity === undefined || entity.id === undefined) {
+      console.error('Update entity error. Entity: ', entity);
+      throw new AppError('Entity could not be updated', 400);
+    }
+
+    return this.getRepository().save(entity);
+  }
+}
+
+export default AbstractService;
--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -1,42 +1,10 @@
 import { getRepository, Repository } from 'typeorm';
-import AppError from '../errors/AppError';
 import Task from '../models/Task';
+import AbstractService from './AbstractService';
 
-class TaskService {
-  private taskRepository: Repository<Task>;
-
-  constructor() {
-    this.taskRepository = getRepository(Task);
-  }
-
-  async remove(taskId: string): Promise<Task> {
-    const task: Task | undefined = await this.taskRepository.findOne(taskId);
-
-    if (task !== undefined) {
-      return this.taskRepository.remove(task);
-    }
-    console.error('Remove task error. Task: ', task);
-    throw new AppError('Task not found', 404);
-  }
-
-  async save(task: Task): Promise<Task> {
-    if (task.id !== undefined) {
-      console.error('Save task error. Task: ', task);
-      throw new AppError('Task already exists', 400);
-    }
-    return this.taskRepository.save(task);
-  }
-
-  async update(task: Task): Promise<Task> {
-    if (task === undefined || task.id === undefined) {
-      console.error('Update task error. Task: ', task);
-      throw new AppError('Task could not be updated', 400);
-    }
-    return this.taskRepository.save(task);
-  }
-
-  async findById(id: string | undefined): Promise<Task | undefined> {
-    return this.taskRepository.findOne(id);
+class TaskService extends AbstractService<Task> {
+  getRepository(): Repository<Task> {
+    return getRepository(Task);
   }
 }
 
